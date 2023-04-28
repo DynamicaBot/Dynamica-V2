11c407f8de7551ddda5f0fda3c74aa22ee02e7fe
Sebastian Pietschner <32213671+sebasptsch@users.noreply.github.com>
2023-04-28 12:07:06 +1000

    feat(sentry): Add sentry integration (#40)

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
@@ -5,6 +6,17 @@ import { PrismaService } from './features/prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = app.get(ConfigService);
+
+  const dsn = config.get('SENTRY_DSN');
+
+  if (dsn) {
+    const sentry = await import('@sentry/node');
+    sentry.init({
+      dsn,
+    });
+  }
+
   app.enableCors({
     origin: '*',
   });
