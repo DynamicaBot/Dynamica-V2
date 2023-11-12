acd740507cad56a56184939892b646de8224cdb1
Sebastian Pietschner <32213671+sebasptsch@users.noreply.github.com>
2023-11-12 16:28:59 +1100

    fix(filters): Discord object not iterable

--- src/filters/DiscordFilter.ts
+++ src/filters/DiscordFilter.ts
@@ -6,7 +6,16 @@ import { NecordArgumentsHost } from 'necord';
 @Catch()
 export class DiscordExceptionFilter implements ExceptionFilter {
   public async catch(exception: Error, host: ArgumentsHost) {
-    const [interaction] = NecordArgumentsHost.create(host).getContext();
+    const necordContext = NecordArgumentsHost.create(host).getContext();
+    if (!Array.isArray(necordContext)) {
+      return console.log(exception);
+    }
+
+    const [interaction] = necordContext;
+
+    if (!interaction) {
+      return console.log(exception);
+    }
 
     Sentry.captureException(exception);
 
