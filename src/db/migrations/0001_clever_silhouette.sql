012eb6edeca5b40eecbdf2427ab876d0e443483d
Sebastian Pietschner <32213671+sebasptsch@users.noreply.github.com>
2023-11-17 12:55:56 +1100

    diy migrations

--- src/db/migrations/0001_clever_silhouette.sql
+++ src/db/migrations/0001_clever_silhouette.sql
@@ -1,57 +1,78 @@
 DROP TABLE `_prisma_migrations`;--> statement-breakpoint
-DROP INDEX IF EXISTS `Alias_guildId_activity_key`;--> statement-breakpoint
-DROP INDEX IF EXISTS `Secondary_guildId_id_key`;--> statement-breakpoint
-DROP INDEX IF EXISTS `Secondary_id_key`;--> statement-breakpoint
-DROP INDEX IF EXISTS `Guild_id_key`;--> statement-breakpoint
+DROP INDEX `Guild_id_key`;--> statement-breakpoint
+DROP INDEX`Alias_guildId_activity_key`;--> statement-breakpoint
+DROP INDEX `Primary_guildId_id_key`;--> statement-breakpoint
+DROP INDEX `Primary_id_key`;--> statement-breakpoint
+DROP INDEX `Secondary_guildId_id_key`;--> statement-breakpoint
+DROP INDEX `Secondary_id_key`;--> statement-breakpoint
 
 -- Add NOT NULL to Secondary.emoji and make Secondary.locked an integer
 PRAGMA foreign_keys=OFF;--> statement-breakpoint
 CREATE TABLE `new_Secondary` (
 	`id` text PRIMARY KEY NOT NULL,
 	`name` text,
-	`creator` text,
+	`owner` text,
 	`emoji` text NOT NULL,
 	`locked` integer NOT NULL,
 	`guildId` text NOT NULL,
 	`primaryId` text NOT NULL,
-    `createdAt` numeric DEFAULT (CURRENT_TIMESTAMP) NOT NULL,
-	`updatedAt` numeric DEFAULT (CURRENT_TIMESTAMP) NOT NULL,
 	`lastName` text DEFAULT '' NOT NULL,
 	FOREIGN KEY (`primaryId`) REFERENCES `Primary`(`id`) ON UPDATE cascade ON DELETE cascade,
 	FOREIGN KEY (`guildId`) REFERENCES `Guild`(`id`) ON UPDATE cascade ON DELETE cascade
 );--> statement-breakpoint
-INSERT INTO "new_Secondary" SELECT `id`, `name`, `creator`, `emoji`, `locked`, `guildId`, `primaryId`, `createdAt`, `updatedAt`, `lastName` FROM "Secondary";--> statement-breakpoint
+INSERT INTO "new_Secondary" SELECT `id`, `name`, `creator` AS `owner`, `emoji`, `locked`, `guildId`, `primaryId`, `lastName` FROM "Secondary";--> statement-breakpoint
 DROP TABLE "Secondary";--> statement-breakpoint
 ALTER TABLE "new_Secondary" RENAME TO "Secondary";--> statement-breakpoint
+PRAGMA foreign_key_check;--> statement-breakpoint
 PRAGMA foreign_keys=ON;--> statement-breakpoint
 
 -- Remove default from Guild.allowJoinRequests and change type from numeric to integer by renaming the column
 PRAGMA foreign_keys=OFF;--> statement-breakpoint
 CREATE TABLE `new_Guild` (
 	`id` text PRIMARY KEY NOT NULL,
-	`allowJoinRequests` integer NOT NULL,
-    `createdAt` numeric DEFAULT (CURRENT_TIMESTAMP) NOT NULL,
-	`updatedAt` numeric DEFAULT (CURRENT_TIMESTAMP) NOT NULL
+	`allowJoinRequests` integer NOT NULL
 ); --> statement-breakpoint
-INSERT INTO "new_Guild" SELECT `id`, `allowJoinRequests`, `updatedAt`, `createdAt` FROM "Guild";--> statement-breakpoint
+INSERT INTO "new_Guild" SELECT `id`, `allowJoinRequests` FROM "Guild";--> statement-breakpoint
 DROP TABLE "Guild";--> statement-breakpoint
 ALTER TABLE "new_Guild" RENAME TO "Guild";--> statement-breakpoint
+PRAGMA foreign_key_check;--> statement-breakpoint
 PRAGMA foreign_keys=ON;--> statement-breakpoint
 
--- Rename creator to owner in Primary
-ALTER TABLE `Secondary` RENAME COLUMN `creator` TO `owner`;--> statement-breakpoint
+-- Drop updatedAt and createdAt from `Alias` by creating a new table
+PRAGMA foreign_keys=OFF;--> statement-breakpoint
+CREATE TABLE `new_Alias` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`activity` text NOT NULL,
+	`alias` text NOT NULL,
+	`guildId` text NOT NULL,
+	FOREIGN KEY (`guildId`) REFERENCES `Guild`(`id`) ON UPDATE cascade ON DELETE cascade
+); --> statement-breakpoint
+INSERT INTO "new_Alias" SELECT `id`, `activity`, `alias`, `guildId` FROM "Alias";--> statement-breakpoint
+DROP TABLE "Alias";--> statement-breakpoint
+ALTER TABLE "new_Alias" RENAME TO "Alias";--> statement-breakpoint
+PRAGMA foreign_key_check;--> statement-breakpoint
+PRAGMA foreign_keys=ON;--> statement-breakpoint
 
--- Re-create unique index on Alias
-CREATE UNIQUE INDEX `Alias_guildId_activity_key` ON `Alias` (`guildId`,`activity`);--> statement-breakpoint
-CREATE UNIQUE INDEX `Secondary_guildId_id_key` ON `Secondary` (`guildId`,`id`);--> statement-breakpoint
-CREATE UNIQUE INDEX `Secondary_id_key` ON `Secondary` (`id`);--> statement-breakpoint
+-- Drop updatedAt and createdAt from `Primary` by creating a new table
+PRAGMA foreign_keys=OFF;--> statement-breakpoint
+CREATE TABLE `new_Primary` (
+	`id` text PRIMARY KEY NOT NULL,
+	`creator` text NOT NULL,
+	`template` text DEFAULT '@@game@@ ##' NOT NULL,
+	`generalName` text DEFAULT 'General ##' NOT NULL,
+	`guildId` text NOT NULL,
+	FOREIGN KEY (`guildId`) REFERENCES `Guild`(`id`) ON UPDATE cascade ON DELETE cascade
+); --> statement-breakpoint
+INSERT INTO "new_Primary" SELECT `id`, `creator`, `template`, `generalName`, `guildId` FROM "Primary";--> statement-breakpoint
+DROP TABLE "Primary";--> statement-breakpoint
+ALTER TABLE "new_Primary" RENAME TO "Primary";--> statement-breakpoint
+PRAGMA foreign_key_check;--> statement-breakpoint
+PRAGMA foreign_keys=ON;--> statement-breakpoint
+
+-- Re-create unique indexes
 CREATE UNIQUE INDEX `Guild_id_key` ON `Guild` (`id`) ;--> statement-breakpoint
--- Drop all createdAt and updatedAt columns
-ALTER TABLE `Alias` DROP COLUMN `createdAt`;--> statement-breakpoint
-ALTER TABLE `Alias` DROP COLUMN `updatedAt`;--> statement-breakpoint
-ALTER TABLE `Guild` DROP COLUMN `createdAt`;--> statement-breakpoint
-ALTER TABLE `Guild` DROP COLUMN `updatedAt`;--> statement-breakpoint
-ALTER TABLE `Primary` DROP COLUMN `createdAt`;--> statement-breakpoint
-ALTER TABLE `Primary` DROP COLUMN `updatedAt`;--> statement-breakpoint
-ALTER TABLE `Secondary` DROP COLUMN `createdAt`;--> statement-breakpoint
-ALTER TABLE `Secondary` DROP COLUMN `updatedAt`;
+CREATE UNIQUE INDEX`Alias_guildId_activity_key` ON `Alias` (`guildId`,`activity`);--> statement-breakpoint
+CREATE UNIQUE INDEX `Primary_guildId_id_key` ON `Primary` (`guildId`,`id`);--> statement-breakpoint
+CREATE UNIQUE INDEX `Primary_id_key` ON `Primary` (`id`);--> statement-breakpoint
+CREATE UNIQUE INDEX `Secondary_guildId_id_key` ON `Secondary` (`guildId`,`id`);--> statement-breakpoint
+CREATE UNIQUE INDEX `Secondary_id_key` ON `Secondary` (`id`);
