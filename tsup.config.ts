1bc0ce87964459399992f6e6ff0789692906d43b
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2024-08-30 15:33:33 +1000

    disable semantic commits and clean tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,18 +1,11 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig(({ watch }) => ({
-	entry: ["src/main.ts", "src/utils/migrateToPostgres.ts"],
-	splitting: true,
+	entry: ["src/main.ts"],
 	sourcemap: true,
 	clean: true,
 	format: ["esm"],
-	platform: "node",
 	minify: false,
 	dts: !!watch,
-	bundle: true,
-	metafile: true,
-	loader: {
-		".graphql": "text",
-	},
 	onSuccess: watch ? "node --enable-source-maps dist/main" : undefined,
 }));
