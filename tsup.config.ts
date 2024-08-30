814cb6dbd6f345f036a7d49a48d83c58b4e16af1
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2024-08-30 15:31:39 +1000

    only emit dts when watching, skip during build

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,18 +1,18 @@
-import { defineConfig } from 'tsup';
+import { defineConfig } from "tsup";
 
 export default defineConfig(({ watch }) => ({
-  entry: ['src/main.ts', 'src/utils/migrateToPostgres.ts'],
-  splitting: true,
-  sourcemap: true,
-  clean: true,
-  format: ['esm'],
-  platform: 'node',
-  minify: false,
-  dts: true,
-  bundle: true,
-  metafile: true,
-  loader: {
-    '.graphql': 'text',
-  },
-  onSuccess: watch ? 'node --enable-source-maps dist/main' : undefined,
+	entry: ["src/main.ts", "src/utils/migrateToPostgres.ts"],
+	splitting: true,
+	sourcemap: true,
+	clean: true,
+	format: ["esm"],
+	platform: "node",
+	minify: false,
+	dts: !!watch,
+	bundle: true,
+	metafile: true,
+	loader: {
+		".graphql": "text",
+	},
+	onSuccess: watch ? "node --enable-source-maps dist/main" : undefined,
 }));
